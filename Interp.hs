794bb0e0af52a0d3c29e4f7474fcee433a0f5388
dizhong <dizhong16@gmail.com>
2016-09-27 12:41:08 -0400

    add tests

--- Interp.hs
+++ Interp.hs
@@ -3,7 +3,7 @@
 module Interp where
 
 data Term a where
-  
+
   Num :: Int
       -> Term Int
   Bool:: Bool
@@ -26,3 +26,13 @@ interp (Bool i)  = i
 interp (Add x y) = interp x + interp y
 interp (Gt  x y) = interp x > interp y
 interp (If x y z) = if (interp x) then (interp y) else (interp z)
+
+
+test0 :: [Bool]
+
+test0 = [ 1     == interp(Num 1),
+          False == interp(Bool False),
+          3     == interp(Add (Num 1) (Num 2)),
+          True  == interp(Gt (Num 3) (Num 2)),
+          True  == interp(If (Bool True) (Bool True) (Bool False)),
+          3     == interp(If (Bool False) (Num 1) (Num 3))]
