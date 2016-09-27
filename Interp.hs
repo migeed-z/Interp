37413e434ccde8e6ed80bed9281b72d559beb20c
migeed-z <migeed.z@outlook.com>
2016-09-27 11:20:11 -0400

    First commit

---
+++ Interp.hs
@@ -0,0 +1,44 @@
+
+
+module Interp where
+
+--data Sty ty  where
+  --TBool :: Sty Bool
+  --TInt  :: Sty Int
+
+
+data Term a where
+  Num :: Int
+      -> Term Int
+  Add :: Term Int
+      -> Term Int
+      -> Term Int
+  Gt  :: Term Int
+      -> Term Int
+      -> Term Bool
+      
+    
+interp :: Term a -> a
+interp (Num i) = i
+interp (Add x y) = interp x + interp y
+interp (Gt  x y) = interp x > interp y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
