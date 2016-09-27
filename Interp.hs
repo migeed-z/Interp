147f8439d926314d9610d3a9540f1fc940ca1a70
dizhong <dizhong16@gmail.com>
2016-09-27 11:55:21 -0400

    bool

--- Interp.hs
+++ Interp.hs
@@ -2,24 +2,22 @@
 
 module Interp where
 
---data Sty ty  where
-  --TBool :: Sty Bool
-  --TInt  :: Sty Int
-
-
 data Term a where
   Num :: Int
       -> Term Int
+  Bool:: Bool
+      -> Term Bool
   Add :: Term Int
       -> Term Int
       -> Term Int
   Gt  :: Term Int
       -> Term Int
       -> Term Bool
-      
-    
+
+
 interp :: Term a -> a
-interp (Num i) = i
+interp (Num i)   = i
+interp (Bool i)  = i
 interp (Add x y) = interp x + interp y
 interp (Gt  x y) = interp x > interp y
 
