c3f954750f4980046f83fe45dc92576f219e4d06
migeed-z <migeed.z@outlook.com>
2016-09-28 12:38:45 -0400

    ..

--- Interp.hs
+++ Interp.hs
@@ -42,3 +42,12 @@ interp (Gt  x y) env = (interp x env) > (interp y env)
 interp (If x y z)env = if (interp x env) then (interp y env) else (interp z env)
 
 
+env0 = []
+test0 :: [Bool]
+
+test0 = [ 1     == interp((Num 1) env0),
+          False == interp((Bool False) env0),
+          3     == interp((Add (Num 1) (Num 2)) env0),
+          True  == interp((Gt (Num 3) (Num 2)) env0),
+          True  == interp((If (Bool True) (Bool True) (Bool False)) env0),
+          3     == interp((If (Bool False) (Num 1) (Num 3)) env0)]
