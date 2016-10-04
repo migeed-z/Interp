01a04925347969c14a2f6eecb9d94a9ca3151c81
dizhong <dizhong16@gmail.com>
2016-10-04 14:57:32 -0400

    add tests

--- Interp.hs
+++ Interp.hs
@@ -5,7 +5,7 @@ module Interp where
 data Term env a where
 
   Num :: Int
-      -> Term env Int  
+      -> Term env Int
   Bool:: Bool
       -> Term env Bool
   Add :: Term env Int
@@ -25,34 +25,36 @@ data Term env a where
   App :: Term env (a -> b)
       -> Term env a
       -> Term env b
-  
+
 data VarT env b where
   Z :: VarT (b, env') b
   S :: VarT env' b
     -> VarT (c, env') b
 
-  
+
 interp :: Term env a -> env ->  a
-interp (Num i) env   = i
-interp (Bool i) env  = i
-interp (Add x y) env = (interp x env) + (interp y env)
-interp (Gt  x y) env= (interp x env) > (interp y env)
-interp (If x y z) env= if (interp x env) then (interp y env) else (interp z env)
-interp (Var x) env = lookupEnv x env
-interp (Lam e) env = \x -> interp e (x, env)
-interp (App f x) env = interp f env (interp x env)
+interp (Num i) env    = i
+interp (Bool i) env   = i
+interp (Add x y) env  = (interp x env) + (interp y env)
+interp (Gt  x y) env  = (interp x env) > (interp y env)
+interp (If x y z) env = if (interp x env) then (interp y env) else (interp z env)
+interp (Var x) env    = lookupEnv x env
+interp (Lam e) env    = \x -> interp e (x, env)
+interp (App f x) env  = interp f env (interp x env)
 
 lookupEnv :: VarT env a
           -> env
           -> a
-lookupEnv Z (a, env) = a
+lookupEnv Z (a, env)     = a
 lookupEnv (S x) (b, env) = lookupEnv x env
 
 test0 :: [Bool]
 
-test0 = [ 1     == interp(Num 1) (),
-          False == interp(Bool False) (),
-          3     == interp(Add (Num 1) (Num 2)) (),
-          True  == interp(Gt (Num 3) (Num 2)) (),
-          True  == interp(If (Bool True) (Bool True) (Bool False)) (),
-          3     == interp(If (Bool False) (Num 1) (Num 3)) ()]
+test0 = [ 1     == interp (Num 1) (),
+          False == interp (Bool False) (),
+          3     == interp (Add (Num 1) (Num 2)) (),
+          True  == interp (Gt (Num 3) (Num 2)) (),
+          True  == interp (If (Bool True) (Bool True) (Bool False)) (),
+          3     == interp (If (Bool False) (Num 1) (Num 3)) (),
+          3     == interp (App (Lam (Var Z)) (Num 3)) (),
+          4     == interp (App (Lam (App (Lam (Var (S Z))) (Num 3))) (Num 4)) ()]
